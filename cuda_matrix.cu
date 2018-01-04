84aead831879f7fd580d719131dc1b356169d903
Wojciech Richert <wojtek@MacBook-Pro-Wojciech.local>
2018-01-04 12:08:58 +0100

    nie wiem co sie zmienilo

--- cuda_matrix.cu
+++ cuda_matrix.cu
@@ -4,34 +4,35 @@
 
 using namespace std;
 
-int transponowanie(){
-  clock_t begin = clock();
-  int const size(1000);
-  static double tablica[size][size];
-  static double tab[size][size];
-  for(int i=0; i<size;i++){
-    for(int j=0; j<size;j++){
-      tablica[i][j]=i*size+j+1;
-    }
-  }
+int transponowanie(double tablica[1000][1000],double tab[1000][1000]){
   printf("przed: 1:%f ", tablica[0][1]);
   printf("2:%f \n", tablica[1][0]);
-  for(int i=0; i<size;i++){
-    for(int j=0; j<size;j++){
+  for(int i=0; i<1000;i++){
+    for(int j=0; j<1000;j++){
       tab[j][i]=tablica[i][j];
     }
   }
   printf("po: 1:%f ", tab[0][1]);
   printf("2:%f \n", tab[1][0]);
-  clock_t end = clock();
-  double elapsed_secs = double(end - begin) / CLOCKS_PER_SEC;
-  printf("czas CPU: %f \n", elapsed_secs);
   return 1;
 }
 
-
+__global__ void transpose(double *matrix){
+}
 
 
 int main(void){
-  transponowanie();
+  clock_t begin = clock();
+  static double tablica[1000][1000];
+  static double tab[1000][1000];
+  for(int i=0; i<1000;i++){
+    for(int j=0; j<1000;j++){
+      tablica[i][j]=i*1000+j+1;
+    }
+  }
+  transponowanie(tablica,tab);
+  clock_t end = clock();
+  double elapsed_secs = double(end - begin) / CLOCKS_PER_SEC;
+  printf("czas CPU: %f \n", elapsed_secs);
+
 }
